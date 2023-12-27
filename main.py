326b04cdb8bb553d0f0eca6d3aa71c6f560b38bc
aziz_go <shakirovazizshavkatovich@gmail.com>
2023-12-27 14:55:31 +0500

    add show products method

--- main.py
+++ main.py
@@ -16,8 +16,31 @@ class Product:
     def get_price(self):
         return self.price
     
+    def __str__(self):
+        return f"""{self.get_name} 
+{self.get_info} 
+{self.get_price}
+"""
+    
 
+class Market:
+    def __init__(self, name, location, phone_number) -> None:
+        self.name = name
+        self.location = location
+        self.phone_number = phone_number
+        self.staff = None
+        self.products = [
+            Product('Lavash', "Hamir, Go'sht", 25000),
+            Product('Hot Dok', "Hamir, Sosiska", 15000),
+            Product('Burger', "Hamir, Go'sht", 35000)
+        ]
 
-p1 = Product('Lavash', "Hamir, Go'sht", 25000)
+    def show_products(self):
+       [print(product) for product in self.products]
 
-print(p1.get_name)
+# p1 = 
+
+# print(p1.get_name)
+
+m1 = Market("EVOS", "Halqobod", "+998977777777")
+m1.show_products()
